b96389506bdc3e3e171c15c63c3d0e72c28be2ad
Qi Zhou <qizhou@quarkchain.org>
2022-09-05 18:39:49 -0700

    add graphics

--- das/das_rec.py
+++ das/das_rec.py
@@ -1,32 +1,49 @@
 import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+
+
+# seed = time.time_ns()
+seed = 1662426016441515000 # for 128x128 with 60% corrupted
+random.seed(seed)
+print("seed {}".format(seed))
+show_plot = False
 
 # numbers of rows and cols of encoded matrix (1/2 RS)
-N_ROWS = 512
-N_COLS = 512
+N_ROWS = 128
+N_COLS = 128
+
+SAMPLE_MISSING = 0
+SAMPLE_RECEIVED = 1
+SAMPLE_RECOVERED = 2
 
 # encoded matrix, False means non-withheld and correct, True means withheld or corrupted
-matrix = [[False] * N_COLS for r in range(N_ROWS)]
+matrix = [[SAMPLE_RECEIVED] * N_COLS for r in range(N_ROWS)]
 row_samples = [N_COLS] * N_ROWS
 col_samples = [N_ROWS] * N_COLS
 
 # number of corrupted samples
-N_CORRUPTED = N_ROWS * N_COLS // 100 * 55
+N_CORRUPTED = N_ROWS * N_COLS // 100 * 60
 # N_CORRUPTED = 48
 n_available = N_ROWS * N_COLS - N_CORRUPTED
 
-# corrupt the samples with EXACT number
+# corrupt/withhold the samples with EXACT number
 for i in range(N_CORRUPTED):
     while True:
         r = random.randint(0, N_ROWS - 1)
         c = random.randint(0, N_COLS - 1)
-        if not matrix[r][c]:
-            matrix[r][c] = True
+        if matrix[r][c] != SAMPLE_MISSING:
+            matrix[r][c] = SAMPLE_MISSING
             row_samples[r] -= 1
             col_samples[c] -= 1
             break
 
 
 tik = 0
+if show_plot:
+    sns.heatmap(matrix, vmin=SAMPLE_MISSING, vmax=SAMPLE_RECOVERED, linewidth=0.5, cbar=False)
+    plt.pause(10)
 while n_available != N_ROWS * N_COLS:
     print("Tik: {}, available samples: {}".format(tik, n_available))
     tik += 1
@@ -42,8 +59,8 @@ while n_available != N_ROWS * N_COLS:
         # reconstruct and re-distribute the samples
         n_rec = 0
         for c in range(N_COLS):
-            if matrix[r][c]:
-                matrix[r][c] = False
+            if matrix[r][c] == SAMPLE_MISSING:
+                matrix[r][c] = SAMPLE_RECOVERED
                 n_rec += 1
                 col_samples[c] += 1
         assert n_rec == N_COLS - row_samples[r]
@@ -61,8 +78,8 @@ while n_available != N_ROWS * N_COLS:
         # reconstruct and re-distribute the samples 
         n_rec = 0
         for r in range(N_ROWS):
-            if matrix[r][c]:
-                matrix[r][c] = False
+            if matrix[r][c] == SAMPLE_MISSING:
+                matrix[r][c] = SAMPLE_RECOVERED
                 n_rec += 1
                 row_samples[r] += 1
         assert n_rec == N_ROWS - col_samples[c]
@@ -71,6 +88,9 @@ while n_available != N_ROWS * N_COLS:
 
     n_available += n_rec_total
     print("Tik: {}, reconstructed samples: {}".format(tik, n_rec_total))
+    if show_plot:
+        ax = sns.heatmap(matrix, vmin=SAMPLE_MISSING, vmax=SAMPLE_RECOVERED, linewidth=0.5, cbar=False)
+        plt.pause(0.2)
     if n_rec_total == 0:
         break
 
@@ -79,5 +99,6 @@ if n_available == N_ROWS * N_COLS:
 else:
     print("Reconstruction failed, remaining samples {}, took {} tiks".format(N_ROWS * N_COLS - n_available, tik))
         
-
+if show_plot:
+    plt.show()
 

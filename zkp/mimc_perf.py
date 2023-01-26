a3f6e50c3352b6f264d350fae1bcd255d58cb281
Qi Zhou <qzhou64@gmail.com>
2023-01-25 16:12:13 -0800

    add VeeDo's VDF

--- zkp/mimc_perf.py
+++ zkp/mimc_perf.py
@@ -15,3 +15,17 @@ print("Forward computation takes: %.4fs" % (time.time() - t))
 t = time.time()
 xs1 = [pow(x3, rm, m) % m for x3 in x3s]
 print("Backward computation takes: %.4fs" % (time.time() - t))
+
+
+# use VeeDo's setup
+m = 0x30000003000000010000000000000001
+rm = (2 * m - 1) // 3
+xs = [random.randint(0, m - 1) for i in range(n)]
+
+t = time.time()
+x3s = [x ** 3 % m for x in xs]
+print("Forward computation takes: %.4fs" % (time.time() - t))
+
+t = time.time()
+xs1 = [pow(x3, rm, m) % m for x3 in x3s]
+print("Backward computation takes: %.4fs" % (time.time() - t))

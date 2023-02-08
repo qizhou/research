04308fc0e95dd45d5b13e93fe7fdb4f9849d1b0c
Qi Zhou <qzhou64@gmail.com>
2023-02-08 14:56:23 -0800

    add prime field for plonk constraint

--- zkp/plonk/constraint.py
+++ zkp/plonk/constraint.py
@@ -1,41 +1,44 @@
 # Constraint utility for PLONK gates like
 # Ql(i) * ai + Qr(i) * bi + Qo(i) * ci + Qm(i) * ai * bi + Qci == 0
 
-def add_add_constraint(Ql, Qr, Qm, Qo, Qc):
+from poly_utils import PrimeField
+
+def add_add_constraint(pf, Ql, Qr, Qm, Qo, Qc):
     # add ai + bi = ci
     Ql.append(1)
     Qr.append(1)
     Qm.append(0)
-    Qo.append(-1)
+    Qo.append(pf.modulus - 1)
     Qc.append(0)
 
-def add_mul_constraint(Ql, Qr, Qm, Qo, Qc):
+def add_mul_constraint(pf, Ql, Qr, Qm, Qo, Qc):
     # add ai * bi = ci
     Ql.append(0)
     Qr.append(0)
     Qm.append(1)
-    Qo.append(-1)
+    Qo.append(pf.modulus - 1)
     Qc.append(0)
 
-def add_const_constraint(Ql, Qr, Qm, Qo, Qc, const):
+def add_const_constraint(pf, Ql, Qr, Qm, Qo, Qc, const):
     # add ci = const_i
     Ql.append(0)
     Qr.append(0)
     Qm.append(0)
     Qo.append(1)
-    Qc.append(-const)
+    Qc.append(-const % pf.modulus)
 
-def is_poly_satisfied(Qli, Qri, Qmi, Qoi, Qci, ai, bi, ci):
-    return Qli * ai + Qri * bi + Qmi * ai * bi + Qoi * ci + Qci == 0
+def is_poly_satisfied(pf, Qli, Qri, Qmi, Qoi, Qci, ai, bi, ci):
+    return (Qli * ai + Qri * bi + Qmi * ai * bi + Qoi * ci + Qci) % pf.modulus == 0
 
-def is_system_satisfied(Ql, Qr, Qm, Qo, Qc, a, b, c):
+def is_system_satisfied(pf, Ql, Qr, Qm, Qo, Qc, a, b, c):
     for Qli, Qri, Qmi, Qoi, Qci, ai, bi, ci in zip(Ql, Qr, Qm, Qo, Qc, a, b, c):
-        if not is_poly_satisfied(Qli, Qri, Qmi, Qoi, Qci, ai, bi, ci):
+        if not is_poly_satisfied(pf, Qli, Qri, Qmi, Qoi, Qci, ai, bi, ci):
             return False
     return True
 
-class PlonkSystem:
-    def __init__(self):
+class PlonkSystem(PrimeField):
+    def __init__(self, modulus):
+        super().__init__(modulus)
         self.Ql = []
         self.Qr = []
         self.Qm = []
@@ -46,21 +49,21 @@ class PlonkSystem:
         self.c = []
 
     def add_add_constraint(self):
-        add_add_constraint(self.Ql, self.Qr, self.Qm, self.Qo, self.Qc)
+        add_add_constraint(self, self.Ql, self.Qr, self.Qm, self.Qo, self.Qc)
 
     def add_mul_constraint(self):
-        add_mul_constraint(self.Ql, self.Qr, self.Qm, self.Qo, self.Qc)
+        add_mul_constraint(self,  self.Ql, self.Qr, self.Qm, self.Qo, self.Qc)
 
     def add_const_constraint(self, const):
-        add_const_constraint(self.Ql, self.Qr, self.Qm, self.Qo, self.Qc, const)
+        add_const_constraint(self, self.Ql, self.Qr, self.Qm, self.Qo, self.Qc, const)
 
     def is_satisfied(self, a, b, c):
-        return is_system_satisfied(self.Ql, self.Qr, self.Qm, self.Qo, self.Qc, a, b, c)
+        return is_system_satisfied(self, self.Ql, self.Qr, self.Qm, self.Qo, self.Qc, a, b, c)
 
 def test_constraint():
     # Simple test for x * x * x + x + 5 == 35
 
-    s = PlonkSystem()
+    s = PlonkSystem(37)
 
     # c0 = a0 * b0 (with a0, b0 = x)
     s.add_mul_constraint()

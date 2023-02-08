6653e39619dc59d1a58330af71a0fb1e4dcb2970
Qi Zhou <qzhou64@gmail.com>
2023-02-08 15:43:04 -0800

    add copy constraint

--- zkp/plonk/constraint.py
+++ zkp/plonk/constraint.py
@@ -47,18 +47,64 @@ class PlonkSystem(PrimeField):
         self.a = []
         self.b = []
         self.c = []
+        self.gateFinalized = False
+        self.copyConstraints = []
 
     def add_add_constraint(self):
+        assert not self.gateFinalized
         add_add_constraint(self, self.Ql, self.Qr, self.Qm, self.Qo, self.Qc)
 
     def add_mul_constraint(self):
+        assert not self.gateFinalized
         add_mul_constraint(self,  self.Ql, self.Qr, self.Qm, self.Qo, self.Qc)
 
     def add_const_constraint(self, const):
+        assert not self.gateFinalized
         add_const_constraint(self, self.Ql, self.Qr, self.Qm, self.Qo, self.Qc, const)
 
-    def is_satisfied(self, a, b, c):
+    def is_gate_satisfied(self, a, b, c):
         return is_system_satisfied(self, self.Ql, self.Qr, self.Qm, self.Qo, self.Qc, a, b, c)
+
+    def is_copy_satisfied(self, a, b, c):
+        l = a + b + c
+        for i in range(len(self.copyConstraints)):
+            if l[i] != l[self.copyConstraints[i]]:
+                return False
+        return True
+
+    def check_copy_unconstraints(self, a, b, c):
+        # given a list of unconstrain variables, check
+        # - they are unconstrained
+        # - rest of them are constrained
+        n = len(self.Ql)
+        unc_set = set(a).union([x+n for x in b]).union([x+2*n for x in c])
+        for i in range(3 *n):
+            if i in unc_set:
+                # the variable should not be copy constrained
+                if self.copyConstraints[i] != i:
+                    return False
+            else:
+                # the variable should be copy constrained
+                if self.copyConstraints[i] == i:
+                    return False
+        return True
+
+
+    def finalize_gate_constraints(self):
+        self.gateFinalized = True
+        self.copyConstraints = [i for i in range(len(self.Ql) * 3)]
+
+    def add_copy_constraint(self, al, bl, cl):
+        assert self.gateFinalized
+        al.sort()
+        bl.sort()
+        cl.sort()
+        n = len(self.Ql)
+        l = al + [x+n for x in bl] + [x+2*n for x in cl]
+        for i, x in enumerate(l):
+            # make sure the copy constraint is not used
+            assert self.copyConstraints[x] == x
+            self.copyConstraints[x] = l[i-1]
 
 def test_constraint():
     # Simple test for x * x * x + x + 5 == 35
@@ -77,6 +123,7 @@ def test_constraint():
     s.add_const_constraint(35)
     # c5 = a5 + b5 (with a5 = c2, b5 = c3, c5 = c4)
     s.add_add_constraint()
+    s.finalize_gate_constraints()
 
     # witness
     x = 3
@@ -84,8 +131,27 @@ def test_constraint():
     a = [x, x * x, x * x * x, anyv, anyv, x * x * x + x]
     b = [x, x, x, anyv, anyv, 5]
     c = [x * x, x * x * x, x * x * x + x, 5, 35, 35]
-    assert s.is_satisfied(a, b, c)
-    print("Constraint test passed")    
+    assert s.is_gate_satisfied(a, b, c)
+    print("Constraint gate test passed")
+
+    # setup copy constraint
+    # a0 = b0 = b1 = b2 = x
+    # a1 = c0 = x ** 2
+    # a2 = c1 = x ** 3
+    # a5 = c2 = x ** 3 + x
+    # b5 = c3
+    # c4 = c5
+    # Q: how about unconstrained variables such as b3, b4, a3, a4?
+    s.add_copy_constraint([0], [0, 1, 2], [])
+    s.add_copy_constraint([1], [], [0])
+    s.add_copy_constraint([2], [], [1])
+    s.add_copy_constraint([5], [], [2])
+    s.add_copy_constraint([], [5], [3])
+    s.add_copy_constraint([], [], [4, 5])
+    assert s.is_copy_satisfied(a, b, c)
+    assert s.check_copy_unconstraints([3, 4], [3, 4], [])
+    print("Constraint copy test passed")
+    
 
 
 if __name__ == "__main__":

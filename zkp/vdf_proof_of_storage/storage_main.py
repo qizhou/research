3799a51b50f3939fbc1621172de717fefb6a8058
Qi Zhou <qzhou64@gmail.com>
2023-03-14 13:57:38 -0700

    add ethash example for proof of storage

--- zkp/vdf_proof_of_storage/storage_main.py
+++ zkp/vdf_proof_of_storage/storage_main.py
@@ -6,12 +6,31 @@ from mimc_stark import mimc_encode, mimc_decode, mk_mimc_proof, verify_mimc_proo
 from hashlib import blake2s
 
 hash_fn = lambda x: blake2s(x).digest()
-N_c = 64        # number of computation trace of VDF (MiMC)
-MIMC_constants = [(i**7) ^ 42 for i in range(min(N_c // 2, 64))] # MiMC round constants
 N_s = 16        # number of samples per solving window
 N_d = 128       # number of data (BLOBs) of F_q
-modulus = 2**256 - 2**32 * 351 + 1      # q value of F_q
-addr = random.randint(0, modulus - 1)   # address of the storage provider
+addr = random.randint(0, 2 ** 20 - 1)   # address of the storage provider (in eth format)
+encode_type = "vde"  # vde or ethash
+
+if encode_type == "vde":
+    print("Using VDE")
+    modulus = 2**256 - 2**32 * 351 + 1      # q value of F_q, 256 bits
+    N_trace = 64        # number of computation trace of VDF (MiMC)
+    MIMC_constants = [(i**7) ^ 42 for i in range(min(N_trace // 2, 64))] # MiMC round constants
+    # encoder and decoder (note that the choice of encoding key may suffer from grinding attack)
+    encode = lambda d, _, addr, idx: mimc_encode((d + addr + idx) % modulus, N_trace, MIMC_constants)
+    decode = lambda d, _, addr, idx: (mimc_decode(d, N_trace, MIMC_constants) - addr - idx) % modulus
+    mk_proof = lambda encoded_sample: mk_mimc_proof(encoded_sample, N_trace, MIMC_constants)
+    verify_proof = lambda encoded_sample, raw_sample, _, addr, idx, proof: verify_mimc_proof(encoded_sample, N_trace, MIMC_constants, (raw_sample + addr + idx) % modulus, proof)
+elif encode_type == "ethash":
+    print("Using Ethash")
+    modulus = 2**256     # Full 32-bytes
+    # encoder and decoder with simple hash (note that the choice of encoding key may suffer from grinding attack)
+    encode = lambda d, h, addr, idx: (d + int.from_bytes(hash_fn(h + addr.to_bytes(20) + idx.to_bytes(8)), byteorder="big")) % modulus
+    decode = lambda d, h, addr, idx: (d - int.from_bytes(hash_fn(h + addr.to_bytes(20) + idx.to_bytes(8)), byteorder="big")) % modulus
+    # TODO
+    mk_proof = lambda encoded_sample: []
+    verify_proof = lambda encoded_sample, raw_sample, _, addr, idx, proof: True
+
 
 # Raw data written by users
 dataset = [random.randint(0, modulus - 1) for i in range(N_d)]
@@ -20,21 +39,22 @@ dataset = [random.randint(0, modulus - 1) for i in range(N_d)]
 hashes = [hash_fn(inp.to_bytes(32, byteorder="big")) for inp in dataset]
 
 # Encoded data (replica) stored by a storage provider (L2 off-chain)
-# The encoding time T_e of each BLOB should be very time consuming so that
+# For VDE encoding, the encoding time T_e of each BLOB should be very time consuming so that
 # - T_e is greater than the duration of solving window (T_w).
 #   This means that an on-demanding encoding cannot produce a candidate within a solving window; or
 # - N_s * T_e > T_w.
 #   This means that an on-demanding encoding must encode all data (or unstored data) to produce a candidate.
+# For Ethash encoding, we just want to make sure the energy cost of encoding a BLOB > sampling cost of a BLOB
 # The encoded data is mixed with addr and index to ensure the encoded replica/BLOB is unique
 print("Encoding")
 start_time = time.time()
-encoded_dataset = [mimc_encode((inp + addr + i) % modulus, N_c, MIMC_constants) for (i, inp) in enumerate(dataset)]
+encoded_dataset = [encode(inp, hashes[i], addr, i) for (i, inp) in enumerate(dataset)]
 print("Encoding use time: %.2fs, avg : %.2f ms" % (time.time() - start_time, (time.time() - start_time) / N_d * 1000))
 
 # Decoding the data for verification.  The decoding time should be must faster than encoding.
 print("Decoding")
 start_time = time.time()
-decoded_dataset = [(mimc_decode(inp, N_c, MIMC_constants) - addr - i) % modulus for (i, inp) in enumerate(encoded_dataset)]
+decoded_dataset = [decode(inp, hashes[i], addr, i) for (i, inp) in enumerate(encoded_dataset)]
 assert decoded_dataset == dataset
 print("Decoding use time: %.2fs, avg : %.2f ms" % (time.time() - start_time, (time.time() - start_time) / N_d * 1000))
 
@@ -60,9 +80,9 @@ def generate_proof(seed, diff, N_s, addr, encoded_dataset):
         if int.from_bytes(candidate, byteorder="big") * diff <= 2 ** 256:
             break
     encoded_samples = [encoded_dataset[p] for p in ps]
-    samples = [(mimc_decode(s, N_c, MIMC_constants) - addr - i) % modulus for i, s in zip(ps, encoded_samples)]
-    stark_proof = [mk_mimc_proof(encoded_sample, N_c, MIMC_constants) for encoded_sample in encoded_samples]
-    proof = (ps, samples, encoded_samples, stark_proof)
+    samples = [decode(s, hashes[i], addr, i) for i, s in zip(ps, encoded_samples)]
+    encoding_proof = [mk_proof(encoded_sample) for encoded_sample in encoded_samples]
+    proof = (ps, samples, encoded_samples, encoding_proof)
     print("Generated proof with %d candidates" % i)
     return seed, proof
 
@@ -79,7 +99,7 @@ def verify(N_s, seed, addr, diff, hashes, proof):
     assert len(pos) == N_s
     # check MIMC using stark
     for i in range(N_s):
-        assert verify_mimc_proof(encoded_samples[i], N_c, MIMC_constants, (samples[i] + addr + pos[i]) % modulus, stark_proof[i])
+        assert verify_proof(encoded_samples[i], samples[i], hashes[i], addr, pos[i], stark_proof[i])
         # assert verify_mimc_proof(encoded_samples[i], N_c, MIMC_constants, mimc_decode(encoded_samples[i], N_c, MIMC_constants), stark_proof[i])
     # check integrity
     assert [hash_fn(sample.to_bytes(32, byteorder="big")) for sample in samples] == [hashes[p] for p in pos]

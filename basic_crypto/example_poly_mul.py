38b181e3fb384fdef704f83e99092459d42131a1
Qi Zhou <qzhou64@gmail.com>
2023-04-27 17:21:53 -0700

    add basic_crypto

---
+++ basic_crypto/example_poly_mul.py
@@ -0,0 +1,35 @@
+import random
+import time
+
+from fft import fft
+from poly_utils import PrimeField
+
+
+# a simple code to demonstrate f(x) = p(x) q(x) using fft
+# degrees of p(x) and q(x)
+d_p = 254
+d_q = 190
+
+modulus = 52435875175126190479447740508185965837690552500527637822603658699938581184513
+pf = PrimeField(modulus)
+# root of unity
+N = 512
+assert d_p + d_q + 1 <= N
+ru = pf.exp(7, (modulus-1) // N)
+
+poly_p = [random.randint(0, modulus - 1) for i in range(d_p + 1)]
+poly_q = [random.randint(0, modulus - 1) for i in range(d_q + 1)]
+
+start = time.time()
+evals_p = fft(poly_p, modulus, ru)
+evals_q = fft(poly_q, modulus, ru)
+evals_f = [pf.mul(x, y) for x, y in zip(evals_p, evals_q)]
+poly_f = fft(evals_f, modulus, ru, inv=True)
+print("fft used time", time.time() - start)
+
+start = time.time()
+poly_f1 = pf.mul_polys(poly_p, poly_q)
+poly_f1 = poly_f1 + [0] * (N - len(poly_f1)) # padding zeros
+print("direct used time", time.time() - start)
+assert poly_f == poly_f1
+
---
+++ basic_crypto/poly_commit.py
@@ -0,0 +1,141 @@
+# A simple polynomial commitment interface
+
+import random
+import hashlib
+
+from fft import fft
+from ec import (G1Generator, G2Generator, default_ec)
+from poly_utils import PrimeField
+from pairing import ate_pairing
+
+class PolyCommitment:
+    def __init__(self, setup=None):
+        assert setup == None
+        self.modulus = default_ec.n
+        self.pf = PrimeField(self.modulus)  # order of Elliptic curve
+        if setup == None:
+            self.secret = random.randint(0, self.pf.modulus - 1)
+            self.G1 = G1Generator()
+            self.G2 = G2Generator()
+            self.setup_vec1 = []
+            self.setup_vec2 = []
+        else:
+            assert False
+
+    def getSetupVector1(self, length):
+        for i in range(len(self.setup_vec1), length):
+            self.setup_vec1.append(self.G1 * (self.secret ** i))
+        return self.setup_vec1[0:length]
+
+    def getSetupVector2(self, length):
+        for i in range(len(self.setup_vec2), length):
+            self.setup_vec2.append(self.G2 * (self.secret ** i))
+        return self.setup_vec2[0:length]
+
+    # get the commitment of a polynomial in evaluation form
+    # return a curve point
+    def getCommitment(self, evals, g):
+        # g - roots of unity
+        coeffs = fft(evals, self.pf.modulus, g, inv=True)
+        sv = self.getSetupVector1(len(coeffs))
+        return sum(s * c for s, c in zip(sv, coeffs))
+
+    def getSingleProofByEvalIdx(self, evals, g, idx):
+        # g - primitive root of unity
+        # order - order of g
+        # n - g^n root of unity
+        # qx = (x^n - 1) / (x-x0)
+        coeffs = fft(evals, self.pf.modulus, g, inv=True)
+        y0 = evals[idx]
+        x0 = self.pf.exp(g, idx)
+        return self.getSingleProofAt(coeffs, x0, y0)
+
+    def getSingleProofAt(self, coeffs, x0, y0):
+        # g - primitive root of unity
+        # order - order of g
+        # n - g^n root of unity
+        # qx = (x^n - 1) / (x-x0)
+        coeffs = coeffs[:]
+        coeffs[0] = coeffs[0] - y0
+        qx = self.pf.div_polys(coeffs, [self.modulus-x0, 1])
+        sv = self.getSetupVector1(len(qx))
+        return sum(s * c for s, c in zip(sv, qx))
+
+    def verifySingleProof(self, commit, proof, x0, y0):
+        sz2 = self.G2 * self.secret + (self.G2 * x0).negate()
+        pair0 = ate_pairing(proof, sz2)
+
+        cy = commit + (self.G1 * y0).negate()
+        pair1 = ate_pairing(cy, self.G2)
+        return pair0 == pair1
+
+
+def test_poly_commitment():
+    pc = PolyCommitment()
+    G = pc.pf.exp(7, (pc.pf.modulus-1) // 4)
+    evals = [235, 2346, 132213, 61232]
+    commit = pc.getCommitment(evals, G)
+    proof = pc.getSingleProofByEvalIdx(evals, G, 1) # w^1
+    assert pc.verifySingleProof(commit, proof, G, 2346)
+    print("poly_commitment test passed")
+
+
+def test_full_poly():
+    # Verify full data blobs in EIP-4844 setup
+    pc = PolyCommitment()
+    G = pc.pf.exp(7, (pc.pf.modulus-1) // 4)
+    evals = [235, 2346, 132213, 61232]
+    commit = pc.getCommitment(evals, G)
+
+    # given evals, check evals matches the commitment with single open
+    # note that we could also do the check by
+    # - find the evals coeffs
+    # - multiple setup s^x G's
+    # which may be expensive in contract.
+    def get_proof(pc, commit, evals):
+        # find a random evaluation point using Fiat-Shamir heuristic
+        # where inputs are commit + evals
+        data = bytes(commit)
+        for eval in evals:
+            # BLS modulus is in 256-bit
+            data += eval.to_bytes(32, byteorder="big")
+        # simple hash to point
+        r = int.from_bytes(hashlib.sha256(data).digest(), byteorder="big") % default_ec.n
+
+        # use barycentric formula to calculate the point with evaluations
+        pf = pc.pf
+        yr = pf.eval_barycentric(r, [pf.exp(G, i) for i in range(4)], evals)
+
+        # get the proof at random r (this part is off-chain)
+        return pc.getSingleProofAt(fft(evals, pf.modulus, G, inv=True), r, yr)
+
+    def verify_proof(pc, commit, evals, proof):
+        # find a random evaluation point using Fiat-Shamir heuristic
+        # where inputs are commit + evals
+        data = bytes(commit)
+        for eval in evals:
+            # BLS modulus is in 256-bit
+            data += eval.to_bytes(32, byteorder="big")
+        # simple hash to point
+        r = int.from_bytes(hashlib.sha256(data).digest(), byteorder="big") % default_ec.n
+
+        # use barycentric formula to calculate the point with evaluations
+        pf = pc.pf
+        yr = pf.eval_barycentric(r, [pf.exp(G, i) for i in range(4)], evals)
+
+        return pc.verifySingleProof(commit, proof, r, yr)
+
+    proof = get_proof(pc, commit, evals)
+
+    # single open to verify
+    assert verify_proof(pc, commit, evals, proof)
+    print("full_poly test passed")
+
+
+if __name__ == "__main__":
+    test_poly_commitment()
+    test_full_poly()
+        
+        
+        
+    

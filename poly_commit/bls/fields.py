b4263653c262d78aee4d4583482192813fbdcf12
“vio1etus” <615836359@qq.com>
2023-06-16 08:50:09 +0800

    fix __radd__ for sum usage

--- poly_commit/bls/fields.py
+++ poly_commit/bls/fields.py
@@ -24,10 +24,8 @@ class Fq:
             return NotImplemented
         return Fq(self.Q, self.value + other.value)
 
-    def __radd__(self, other: Fq) -> Fq:
-        if not isinstance(other, Fq):
-            return NotImplemented
-        return self.__add__(other)
+    def __radd__(self, other: int) -> Fq:
+        return self.__add__(Fq(self.Q, other))
 
     def __sub__(self, other: Fq) -> Fq:
         if not isinstance(other, Fq):

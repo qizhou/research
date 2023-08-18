cdb52deacb09684d273945cc53ce504365463a95
Qi Zhou <qizhou@ethstorage.io>
2023-08-18 10:51:53 -0700

    fix kad.py

--- kad/kad.py
+++ kad/kad.py
@@ -3,7 +3,7 @@ import heapq
 
 # bucket size
 # K = 20 # default
-K = 20
+K = 4
 
 # number of bits in id
 ID_BIT_SIZE = 16
@@ -37,6 +37,12 @@ class Node:
     def createRandomNode():
         return Node(random.randint(0, ID_MAX))
 
+    def getRandomNodeIds(n):
+        s = set()
+        while len(s) < n:
+            s.add(random.randint(0, ID_MAX))
+        return s
+
     def addNode(self, node):
         dist = log_distance(self.id, node.id) - 1
 
@@ -44,6 +50,7 @@ class Node:
             self.routingTable[dist].append(node)
         else:
             # print("warning: routing table is full")
+            # or randomly drop a node
             pass
 
     def findNode(self, key):
@@ -66,7 +73,7 @@ class Node:
                 counter += 1
                 if len(pq) < K:
                     heapq.heappush(pq, (-dist0, counter, n))
-                else:
+                elif dist0 < -heapq[0][0]:
                     heapq.heappushpop(pq, (-dist0, counter, n))
         return pq
 
@@ -79,45 +86,47 @@ class Node:
             return self
 
         iter = 0
-        print("iter %d" % iter)
+        print("iter %d, distance %d" % (iter, dist))
         pq = self.findNode(key)
         if len(pq) == 1 and pq[0][0] == 0:
             return pq[0][2]
         while True:
             iter += 1
-            print("iter %d" % iter)
+            print("iter %d, nodes %s" % (iter, [(x[2].id, -x[0]) for x in pq]))
             npq = []
             counter = 0
             for n in pq:
                 nodes = n[2].findNode(key)
-                for dist0, _, n in nodes:
-                    if dist0 == 0:
+                for ndist0, _, n in nodes:
+                    if ndist0 == 0:
                         return n
                     if n.id in visited:
                         continue
                     visited.add(n.id)
 
                     counter += 1
-                    if len(pq) < K:
-                        heapq.heappush(npq, (-dist0, counter, n))
-                    else:
-                        heapq.heappushpop(npq, (-dist0, counter, n))
-            
+                    if len(npq) < K:
+                        heapq.heappush(npq, (ndist0, counter, n))
+                    elif -ndist0 < -npq[0][0]:
+                        heapq.heappushpop(npq, (ndist0, counter, n))
+
             if len(npq) == 0:
                 return None
 
             pq = npq
-        
+
 
 random.seed(12345)
-nodes = [Node.createRandomNode() for i in range(1000)]
+nodes = [Node(i) for i in Node.getRandomNodeIds(1000)]
 for i in range(len(nodes)):
     for j in range(i+1, len(nodes)):
         nodes[i].addNode(nodes[j])
         nodes[j].addNode(nodes[i])
 
-print(nodes[0].findNode(nodes[99].id))
-print(nodes[0].findNodeExact(nodes[99].id).id)
-print(nodes[0].findNodeExact(nodes[55].id).id)
-print(nodes[0].findNodeExact(nodes[2].id).id)
-print(nodes[0].findNodeExact(nodes[500].id).id)
+# print(nodes[0].findNode(nodes[99].id))
+# print(nodes[0].findNodeExact(nodes[99].id).id)
+# print(nodes[0].findNodeExact(nodes[55].id).id)
+# print(nodes[0].findNodeExact(nodes[2].id).id)
+# print(nodes[0].findNodeExact(nodes[500].id).id)
+print(nodes[0].findNodeExact(nodes[987].id).id)
+print(nodes[0].findNodeExact(nodes[999].id).id)

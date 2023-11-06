9a7e001bc639ce919b023ede2be2455d8c22f315
Qi Zhou <qizhou@ethstorage.io>
2023-11-06 13:54:49 -0800

    add unshuffle

--- basic_crypto/shuffle.py
+++ basic_crypto/shuffle.py
@@ -1,12 +1,21 @@
 import hashlib
 
-seed = b"abc"
-length = 32
-shuffle = [i + 1 for i in range(32)]
+seed = input("input seed")
+seed1 = input("input seed again")
+if seed != seed1:
+    print("incorrect seed")
+seed = bytes(seed, "ascii")
+
+length = 24
+shuffle = [i + 1 for i in range(length)]
 
 for i in range(length):
     seed = hashlib.sha256(seed).digest()
     x = int.from_bytes(seed, "big") % (length - i) + i
     shuffle[i], shuffle[x] = shuffle[x], shuffle[i]
 
-print(shuffle)
+print([(i + 1, x) for i, x in enumerate(shuffle)])
+unshuffle = [0] * length
+for i, x in enumerate(shuffle):
+    unshuffle[x-1] = i + 1
+print([(i + 1, x) for i, x in enumerate(unshuffle)])

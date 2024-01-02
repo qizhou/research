6bb401dd69e065e9a86b498b405a58ec54b59efe
Qi Zhou <qizhou@ethstorage.io>
2024-01-01 21:59:36 -0800

    add opt w-ots

--- w-ots/sign_and_verify_opt.py
+++ w-ots/sign_and_verify_opt.py
@@ -1,45 +1,46 @@
 # https://asecuritysite.com/encryption/wint
 
 import hashlib
-from binascii import unhexlify, hexlify
-
 
 from os import urandom
 
 import sys
 message="Hello"
 
-def random_key(n=32):                   #returns a 256 bit hex encoded (64 bytes) random number
-    return hexlify(urandom(n))
-
 def sha256(message):
-    return hashlib.sha256(message.encode()).hexdigest()
+    return hashlib.sha256(message).digest()
 
-def sha256b(message):
-    return hashlib.sha256(message.encode()).digest()
+def merkle(hash_list):
+    if (len(hash_list) - 1) & len(hash_list) != 0:
+        raise RuntimeError("hash list must be 2^n")
+    while len(hash_list) != 1:
+        hash_list = [sha256(hash_list[i] + hash_list[i + 1]) for i in range(0, 2, len(hash_list))]
+    return hash_list[0]
 
-def random_wkey(w=8, verbose=0):      #create random W-OTS keypair
+def random_wkey(w=16, verbose=0):      #create random W-OTS keypair
 
     priv = []
     pub = []
     print("Hashing number random keys by:\t",2**w)
     for x in range(256//w):
-        a = str(random_key())
+        a = urandom(w)
         priv.append(a)
         for y in range(2**w-1):              
             a = sha256(a)
         pub.append(sha256(a))               
 
-    return priv, pub
+    return priv, pub 
 
 def sign_wkey(priv, message):      
 
     signature = []
-    bin_msg = unhexlify(sha256(message))
+    bin_msg = sha256(message)
 
+    l = 256 // len(priv) // 8
     for y in range(len(priv)):
-        s = priv[y]    
-        for x in range(256-ord(bin_msg[y:y+1])):
+        s = priv[y]
+        v = int.from_bytes(bin_msg[l*y:y*l+l], 'big')
+        for x in range(256**l-v):
             s = sha256(s)
         signature.append(s)
     return signature
@@ -47,17 +48,18 @@ def sign_wkey(priv, message):
 def verify_wkey(signature, message, pub):
 
     verify = []
-    bin_msg = unhexlify(sha256(message))
+    bin_msg = sha256(message)
+
+    l = 256 // len(priv) // 8
     
     for x in range(len(signature)):
         a = signature[x]
-                                                    #f is all but last hash..
-        for z in range(ord(bin_msg[x:x+1])):
-                a=sha256(a)
-        #a = sha256(a)                               #g is the final hash, separate so can be changed..
+        v = int.from_bytes(bin_msg[l*x:l*x+l], 'big')
+        for z in range(v):
+            a=sha256(a)
         verify.append(a)
   
-    if pub != verify:
+    if pub != merkle(verify):
         return False
 
     return True
@@ -74,21 +76,19 @@ print("Priv[4]: ",priv[4])
 print("Priv[5]: ",priv[5])
 
 print("==== Public key (show everyone)=====")
-print("Pub[0]: ",pub[0])
-print("Pub[1]: ",pub[1])
-print("Pub[2]: ",pub[2])
-print("Pub[3]: ",pub[3])
-print("Pub[4]: ",pub[4])
-print("Pub[5]: ",pub[5])
+pub = merkle(pub)
+print("Pub: ", pub)
 
 print("==== Message to sign ===============")
 print("Message:\t",message)
-print("SHA-256:\t",sha256(message))
+print("SHA-256:\t",sha256(message.encode()))
 print("==== Signature =====================")
-sign = sign_wkey(priv,message)
+sign = sign_wkey(priv, message.encode())
 
+print("Signature size is ", 32 * len(sign))
 print("Sign[0]:\t",sign[0])
 print("Sign[1]:\t",sign[1])
 print("Sign[2]:\t",sign[2])
 print("Sign[3]:\t",sign[3])
-print("The signature test is ",verify_wkey(sign,message,pub))
+print("Sign[{}]:\t".format(len(sign) - 1), sign[-1])
+print("The signature test is ",verify_wkey(sign, message.encode(),pub))

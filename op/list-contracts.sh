8c819a5472d092ff4271990559647d8334d933d8
Qi Zhou <qizhou@ethstorage.io>
2024-10-03 16:41:25 +0800

    update list-contracts.sh

--- op/list-contracts.sh
+++ op/list-contracts.sh
@@ -1,6 +1,12 @@
 
 SYS_CONFIG=0x229047fed2591dbec1eF1118d64F7aF3dB9EB290
 
-echo disputeGameFactory: $(cast call $SYS_CONFIG 'disputeGameFactory()' | cut -b 1,2,27-66)
-echo optimismPortal: $(cast call $SYS_CONFIG 'optimismPortal()' | cut -b 1,2,27-66)
+function print_contract() {
+    echo $1: $(cast call $SYS_CONFIG $1\(\) | cut -b 1,2,27-66)
+}
 
+print_contract disputeGameFactory
+print_contract optimismPortal
+print_contract batcherHash
+print_contract batchInbox
+print_contract l1StandardBridge

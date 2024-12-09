04b322433643a9e0962501747c100f9f2d6f907b
Qi Zhou <qizhou@ethstorage.io>
2024-12-10 01:44:40 +0800

    better list-contracts.sh

--- op/list-contracts.sh
+++ op/list-contracts.sh
@@ -1,5 +1,8 @@
 
-SYS_CONFIG=0x229047fed2591dbec1eF1118d64F7aF3dB9EB290
+SYS_CONFIG=${SYS_CONFIG:-0x229047fed2591dbec1eF1118d64F7aF3dB9EB290}
+
+echo "Config:" $SYS_CONFIG
+echo "RPC:" $ETH_RPC_URL
 
 function print_contract() {
     echo $1: $(cast call $SYS_CONFIG $1\(\) | cut -b 1,2,27-66)

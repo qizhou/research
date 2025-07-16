7476dc256bb92351f6d06e161a8567011081bbd0
Qi Zhou <qizhou@ethstorage.io>
2025-07-16 23:57:43 +0000

    adjust run_bench_large_read.sh

--- kvs_bench/run_bench_large_read.sh
+++ kvs_bench/run_bench_large_read.sh
@@ -2,9 +2,9 @@
 
 set -x
 
-# 1B entries
+# 2B entries with ~110 size to mimic geth usage
 db=pebble
-rm -r bench_*; go run main.go -t 16 -op randwrite -n 10000000000 -S 100 --dbn 1 --db $db
+rm -r bench_*; go run main.go -t 16 -op randwrite -n 2000000000 -S 170 --dbn 1 --db $db -r 0
 for t in 1 2 4 8 16 32 64; do
-  go run main.go -r 0 -t $t -op randread -n 10000000 -S 100 --dbn 1 --db $db
+  go run main.go -r 0 -t $t -op randread -n 10000000 -S 170 --dbn 1 --db $db
 done

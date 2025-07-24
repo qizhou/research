5201001f5952ed48177102b60185e536f36ec303
Qi Zhou <qizhou@ethstorage.io>
2025-07-24 03:09:27 +0000

    hashed.py supports multi-version leaves of the same key

--- trie/hash.py
+++ trie/hash.py
@@ -25,24 +25,24 @@ def get_nbit(key, pos):
 #  root='0'*32
 #
 # b), put(h0,d0) h0=0b010....
-#    root=internal(h0 + '0'*32)
-#  h0=leaf(d0)
+#    root=internal(n0 + '0'*32)
+#  n0=leaf(h0+d0)
 #
 # c), put(h1,d1) h1=0b00...
 #       root=internal(inode0 + '0'*32)
-#  inode0=internal(h1 + h0)
-#  h0=leaf(d0), h1=leaf(d1)
+#  inode0=internal(n1 + n0)
+#  n0=leaf(h0+d0), n1=leaf(h1+d1)
 #
 # d), put(h2,d2) h2=0b11...
-#       root=internal(inode0 + h2)
-#  inode0=internal(h1 + h0)
-#  h0=leaf(d0), h1=leaf(d1), h2=leaf(d2)
+#       root=internal(inode0 + n2)
+#  inode0=internal(n1 + n0)
+#  n0=leaf(h0+d0), n1=leaf(h1+d1), n2=leaf(h2+d2)
 
 # f), from b), put(h1,d1) h1=0b011
 #  root=internal(inode0 + '0'*32)
 #  inode0=internal('0'*32 + inode1)
-#  inode1=internal(h0 + h1)
-#  h0=leaf(d0), h1=leaf(d1)
+#  inode1=internal(n1 + n0)
+#  n0=leaf(h0+d0), n1=leaf(h1+d1)
 
 
 class Tree:
@@ -57,13 +57,13 @@ class Tree:
         return key, node
 
     def __createLeaf(self, key, value):
-        node = Node(KIND_LEAF, value)
-        self.kv[key] = node
-        return node
+        node = Node(KIND_LEAF, key + value) # TODO: only store the part of the key not used in path
+        hkey = hash_func(key + value)
+        self.kv[hkey] = node
+        return hkey, node
 
     def put(self, key, value):
-        # TODO: actual key should be hashed
-        self.__createLeaf(key, value)
+        leaf_hash, leaf_node = self.__createLeaf(key, value)
         node_key = self.root
         path = []
 
@@ -71,15 +71,22 @@ class Tree:
             node = self.kv[node_key]
             if node.kind == KIND_LEAF:
                 # need to create internal nodes to replace the leaf and update following the path
+                existing_leaf_key = node.data[:32]
+                if existing_leaf_key == key:
+                    # update
+                    node_key = leaf_hash
+                    node = leaf_node
+                    break
+
                 depth = len(path)
                 for common in range(depth, 256):
-                    if get_nbit(key, depth) != get_nbit(node_key, depth):
+                    if get_nbit(key, depth) != get_nbit(existing_leaf_key, depth):
                         break
 
                 if get_nbit(key, depth) == 0:
-                    inode_data = key + node_key
+                    inode_data = leaf_hash + node_key
                 else:
-                    inode_data = node_key + key
+                    inode_data = node_key + leaf_hash
                 inode_key, _ = self.__createInternal(inode_data)
                 for i in range(common-1, depth, -1):
                     if get_nbit(key, i) == 0: 
@@ -95,9 +102,9 @@ class Tree:
             if next_node_key == KEY_NIL:
                 # replace the empty slot of the current node and update following the path
                 if node.data[0:32] == KEY_NIL:
-                    new_data = key + node.data[32:]
+                    new_data = leaf_hash + node.data[32:]
                 else:
-                    new_data = node.data[:32] + key
+                    new_data = node.data[:32] + leaf_hash
                 node_key, _ = self.__createInternal(new_data)
                 break
             node_key = next_node_key
@@ -122,8 +129,8 @@ class Tree:
                 return None
             node = self.kv[node_key]
             if node.kind == KIND_LEAF:
-                if node_key == key:
-                    return node.data
+                if node.data[:32] == key:
+                    return node.data[32:]
                 else:
                     return None
             idx = get_nbit(key, i)
@@ -149,6 +156,18 @@ assert t.get(b'\2'+b'\1'*31) == b'\0'
 assert t.get(b'\0'+b'\1'*31) == b'\1'
 assert t.get(b'\3'+b'\1'*31) == b'\2'
 assert t.get(b'\131'+b'\1'*31) == b'\3'
+t.setRoot(root)
+assert t.get(b'\2'+b'\1'*31) == b'\0'
+assert t.get(b'\0'+b'\1'*31) == b'\1'
+assert t.get(b'\3'+b'\1'*31) == b'\2'
+assert t.get(b'\131'+b'\1'*31) == None
+
+# test multi-version
+t.put(b'\0'+b'\1'*31, b'\3')
+assert t.get(b'\2'+b'\1'*31) == b'\0'
+assert t.get(b'\0'+b'\1'*31) == b'\3'
+assert t.get(b'\3'+b'\1'*31) == b'\2'
+assert t.get(b'\131'+b'\1'*31) == None
 t.setRoot(root)
 assert t.get(b'\2'+b'\1'*31) == b'\0'
 assert t.get(b'\0'+b'\1'*31) == b'\1'

faf5be9deb2ed237921993b49a90f1a2773bf1c7
Qi Zhou <qizhou@ethstorage.io>
2025-08-14 17:42:39 +0000

    add jit dynamic load code

--- jit/go-jit/main.go
+++ jit/go-jit/main.go
@@ -36,7 +36,9 @@ func main() {
 	// Write to files
 	// Write to object file
 	compileAndRunFib()
+	compileAndRunFrac()
 	loadFib()
+	loadDynamic()
 }
 
 func loadFib() {
@@ -71,6 +73,158 @@ func loadFib() {
 
 	res := new(big.Int).SetBytes(reverse(buf[:]))
 	fmt.Printf("llvm getFunctionAddress(): fib(%d) = %s, used time %d ns\n", input, res.String(), time.Now().Sub(now).Nanoseconds())
+}
+
+func loadDynamic() {
+	llvm.InitializeNativeTarget()
+	llvm.InitializeNativeAsmPrinter()
+
+	ctx := llvm.NewContext()
+	module := ctx.NewModule("fib_module")
+
+	engine, err := llvm.NewJITCompiler(module, 3)
+	if err != nil {
+		panic(fmt.Sprintf("Error creating JIT: %s", err))
+	}
+	defer engine.Dispose()
+
+	pointer := engine.GetFunctionAddress("fib")
+	fmt.Println("fib func pointer", pointer)
+
+	fmt.Println("loading fib.o")
+	now := time.Now()
+	engine.AddObjectFileByFilename("fib.o")
+	fmt.Printf("loaded used time %d ns\n", time.Now().Sub(now).Nanoseconds())
+
+	pointer = engine.GetFunctionAddress("fib")
+	fmt.Println("fib func pointer", pointer)
+
+	input := 10001
+	now = time.Now()
+
+	var buf [32]byte
+	C.call_fib(C.uint64_t(pointer), C.uint32_t(input), (*C.uchar)(unsafe.Pointer(&buf[0])))
+
+	res := new(big.Int).SetBytes(reverse(buf[:]))
+	fmt.Printf("llvm getFunctionAddress(): fib(%d) = %s, used time %d ns\n", input, res.String(), time.Now().Sub(now).Nanoseconds())
+
+	fmt.Println("loading frac.o")
+	now = time.Now()
+	engine.AddObjectFileByFilename("frac.o")
+	fmt.Printf("loaded used time %d ns\n", time.Now().Sub(now).Nanoseconds())
+
+	pointer = engine.GetFunctionAddress("frac")
+	fmt.Println("fib func pointer", pointer)
+
+	input = 10
+	now = time.Now()
+
+	C.call_fib(C.uint64_t(pointer), C.uint32_t(input), (*C.uchar)(unsafe.Pointer(&buf[0])))
+
+	res = new(big.Int).SetBytes(reverse(buf[:]))
+	fmt.Printf("llvm getFunctionAddress(): frac(%d) = %s, used time %d ns\n", input, res.String(), time.Now().Sub(now).Nanoseconds())
+}
+
+func compileAndRunFrac() {
+	llvm.InitializeNativeTarget()
+	llvm.InitializeNativeAsmPrinter()
+
+	ctx := llvm.NewContext()
+	module := ctx.NewModule("frac_module")
+
+	uint_type := ctx.IntType(256)
+	fib_args := []llvm.Type{ctx.Int32Type(), llvm.PointerType(uint_type, 0)}
+	fib_type := llvm.FunctionType(ctx.VoidType(), fib_args, false)
+	fib_func := llvm.AddFunction(module, "frac", fib_type)
+	fib_func.SetFunctionCallConv(llvm.CCallConv)
+	n := fib_func.Param(0)
+	out := fib_func.Param(1)
+
+	entry := llvm.AddBasicBlock(fib_func, "entry")
+	loop_block := llvm.AddBasicBlock(fib_func, "loop")
+	loop_body_block := llvm.AddBasicBlock(fib_func, "loop_body")
+	after_loop_block := llvm.AddBasicBlock(fib_func, "after_loop")
+
+	builder := ctx.NewBuilder()
+	defer builder.Dispose()
+
+	builder.SetInsertPointAtEnd(entry)
+	a_ptr := builder.CreateAlloca(uint_type, "a")
+	i_ptr := builder.CreateAlloca(ctx.Int32Type(), "i")
+	builder.CreateStore(llvm.ConstInt(uint_type, 1, false), a_ptr)
+	builder.CreateStore(llvm.ConstInt(ctx.Int32Type(), 2, false), i_ptr)
+	builder.CreateBr(loop_block)
+
+	builder.SetInsertPointAtEnd(loop_block)
+	i_val := builder.CreateLoad(ctx.Int32Type(), i_ptr, "i_val")
+	cond := builder.CreateICmp(llvm.IntULE, i_val, n, "loopcond")
+	builder.CreateCondBr(cond, loop_body_block, after_loop_block)
+
+	builder.SetInsertPointAtEnd(loop_body_block)
+	a_val := builder.CreateLoad(uint_type, a_ptr, "a_val")
+	i_ext_val := builder.CreateZExt(i_val, uint_type, "i_ext_val")
+	next_a := builder.CreateMul(a_val, i_ext_val, "next_a")
+	builder.CreateStore(next_a, a_ptr)
+	next_i := builder.CreateAdd(i_val, llvm.ConstInt(ctx.Int32Type(), 1, false), "next_i")
+	builder.CreateStore(next_i, i_ptr)
+	builder.CreateBr(loop_block)
+
+	builder.SetInsertPointAtEnd(after_loop_block)
+	result := builder.CreateLoad(uint_type, a_ptr, "result")
+	builder.CreateStore(result, out)
+
+	builder.CreateRetVoid()
+
+	err := llvm.VerifyModule(module, llvm.ReturnStatusAction)
+	if err != nil {
+		panic(fmt.Sprintf("failed to verify module: %s", err))
+	}
+
+	engine, err := llvm.NewJITCompiler(module, 3)
+	if err != nil {
+		panic(fmt.Sprintf("Error creating JIT: %s", err))
+	}
+	defer engine.Dispose()
+
+	pointer := engine.GetFunctionAddress("frac")
+
+	input := 10
+	now := time.Now()
+
+	var buf [32]byte
+	C.call_fib(C.uint64_t(pointer), C.uint32_t(input), (*C.uchar)(unsafe.Pointer(&buf[0])))
+
+	res := new(big.Int).SetBytes(reverse(buf[:]))
+	fmt.Printf("llvm getFunctionAddress(): frac(%d) = %s, used time %d ns\n", input, res.String(), time.Now().Sub(now).Nanoseconds())
+
+	os.WriteFile("frac.ll", []byte(module.String()), 0644)
+
+	target, err := llvm.GetTargetFromTriple(llvm.DefaultTargetTriple())
+	if err != nil {
+		panic(err)
+	}
+
+	targetMachine := target.CreateTargetMachine(
+		llvm.DefaultTargetTriple(), "generic", "", llvm.CodeGenLevelDefault,
+		llvm.RelocDefault, llvm.CodeModelDefault)
+
+	mem, err := targetMachine.EmitToMemoryBuffer(module, llvm.ObjectFile)
+	if err != nil {
+		panic(err)
+	}
+	os.WriteFile("frac.o", mem.Bytes(), 0644)
+
+	mem, err = targetMachine.EmitToMemoryBuffer(module, llvm.AssemblyFile)
+	if err != nil {
+		panic(err)
+	}
+	os.WriteFile("frac.asm", mem.Bytes(), 0644)
+
+	file, err := os.OpenFile("frac.bc", os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		panic(err)
+	}
+	llvm.WriteBitcodeToFile(module, file)
 }
 
 func compileAndRunFib() {

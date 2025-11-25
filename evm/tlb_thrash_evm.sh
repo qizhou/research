6e075f9f9bf967b0ec855e9764c1a491cbb887eb
Qi Zhou <qizhou@quarkchain.org>
2025-11-25 18:20:53 +0000

    better script for evm memory test

--- evm/tlb_thrash_evm.sh
+++ evm/tlb_thrash_evm.sh
@@ -3,15 +3,11 @@ set -x
 
 gcc -O2 -std=c11 -pthread tlb_thrash_evm.c -o tlb_thrash_evm
 
-# random page read (single page)
-taskset -c 1 perf stat -e dTLB-loads,dTLB-load-misses,cycles,instructions ./tlb_thrash_evm 640 200000 0
-taskset -c 1 perf stat -e dTLB-loads,dTLB-load-misses,cycles,instructions ./tlb_thrash_evm 65536 2000 0
-taskset -c 1 perf stat -e dTLB-loads,dTLB-load-misses,cycles,instructions ./tlb_thrash_evm 1048576 100 0
-# random page read (cross two pages)
-taskset -c 1 perf stat -e dTLB-loads,dTLB-load-misses,cycles,instructions ./tlb_thrash_evm 640 200000 4080
-taskset -c 1 perf stat -e dTLB-loads,dTLB-load-misses,cycles,instructions ./tlb_thrash_evm 65536 2000 4080
-taskset -c 1 perf stat -e dTLB-loads,dTLB-load-misses,cycles,instructions ./tlb_thrash_evm 1048576 100 4080
-# random page read (cross two pages with unaligned 8-byte offset)
-taskset -c 1 perf stat -e dTLB-loads,dTLB-load-misses,cycles,instructions ./tlb_thrash_evm 640 200000 4081
-taskset -c 1 perf stat -e dTLB-loads,dTLB-load-misses,cycles,instructions ./tlb_thrash_evm 65536 2000 4081
-taskset -c 1 perf stat -e dTLB-loads,dTLB-load-misses,cycles,instructions ./tlb_thrash_evm 1048576 100 4081
+for page_num in 16 64 256 640 6400 65536 262144 1048576 4194304 16777216; do
+    # random page read (single page)
+    taskset -c 1 perf stat -e dTLB-loads,dTLB-load-misses,cycles,instructions ./tlb_thrash_evm ${page_num} $((100000000/${page_num})) 0
+    # random page read (cross two pages)
+    taskset -c 1 perf stat -e dTLB-loads,dTLB-load-misses,cycles,instructions ./tlb_thrash_evm ${page_num} $((100000000/${page_num})) 4080
+    # random page read (cross two pages with unaligned 8-byte offset)
+    taskset -c 1 perf stat -e dTLB-loads,dTLB-load-misses,cycles,instructions ./tlb_thrash_evm ${page_num} $((100000000/${page_num})) 4081
+done
